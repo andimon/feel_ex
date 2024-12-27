446a4e463178fe6d21354decf67e6e1572ebd30c
andimon <andrevella2001@gmail.com>
2024-12-27 23:34:44 +0100

    feat: add example usage

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # FeelEx
 
-A friendly expression language helps users define decision logic without needing deep technical expertise. This language is based on the Friendly Enough Expression Language. For more information regarding FEEL, please take a look at the official OMG specification at https://www.omg.org/dmn/.
+A friendly expression language helps users define decision logic without needing deep technical expertise. This language is based on the FEEL(Friendly Enough Expression Language). For more information regarding FEEL, please take a look at the official OMG specification at https://www.omg.org/dmn/.
 
 # Example Usage
 
--- lib/feel_ex.ex
+++ lib/feel_ex.ex
@@ -1,6 +1,17 @@
 defmodule FeelEx do
+  @moduledoc """
+    A friendly expression language helps users define decision logic without needing deep technical expertise. This language is based on the FEEL(Friendly Enough Expression Language). For more information regarding FEEL, please take a look at the official OMG specification at https://www.omg.org/dmn/.
+  """
+
   require Logger
   alias FeelEx.{Helper, Lexer, Parser, Expression}
+
+  @doc """
+  ## Examples
+
+    iex> FeelEx.evaluate(%{a: 1, b: 2},"a+b")
+    %FeelEx.Value{value: 3, type: :number}
+  """
 
   def evaluate(context, feel_expression) when is_map(context) and is_binary(feel_expression) do
     tokens = Lexer.tokens(feel_expression)
